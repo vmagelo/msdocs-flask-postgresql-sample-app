3220f03c76f75ed4f944298647b84b6905d5b537
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-02-27 20:40:29 +0100

    Add restaurant review pieces.

--- azureproject/development.py
+++ azureproject/development.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-from config.production import DATABASE_URI
+from azureproject.production import DATABASE_URI
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
