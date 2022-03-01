3c8f8c9a73c2ce5268da196e427525f1d326da9a
vmagelo <51368918+vmagelo@users.noreply.github.com>
2022-03-01 14:26:25 +0100

    Remove todo - migration folder okay.

--- restaurant_review/app.py
+++ restaurant_review/app.py
@@ -27,9 +27,6 @@ app.config.update(
 
 # initialize the database connection
 db = SQLAlchemy(app)
-#from restaurant_review.models import Review, Restaurant
-#db.create_all()
-#db.session.commit()
 
 # initialize database migration management
 migrate = Migrate(app, db)
